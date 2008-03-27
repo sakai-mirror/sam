0018059058e5d10604482fd80af778ec362bba3b
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-03-27 09:06:03 +0000

    svn log -r41782 https://source.sakaiproject.org/svn/sam/trunk ------------------------------------------------------------------------ r41782 | ktsao@stanford.edu | 2008-03-01 01:36:35 +0200 (Sat, 01 Mar 2008) | 1 line
    SAK-13121
    ------------------------------------------------------------------------
    dhorwitz@david-horwitz-6:~/branchManagemnt/sakai_2-5-x> svn merge -c41782 https://source.sakaiproject.org/svn/sam/trunk sam
    U    sam/samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentGradingFacadeQueries.java
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/sam/branches/sakai_2-5-x@43711 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentGradingFacadeQueries.java
+++ samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentGradingFacadeQueries.java
@@ -438,7 +438,7 @@ public class AssessmentGradingFacadeQueries extends HibernateDaoSupport implemen
 
   public HashMap getSubmissionSizeOfAllPublishedAssessments(){
     HashMap h = new HashMap();
-    Object [] values = {"VIEW_PUBLISHED_ASSESSMENT", AgentFacade.getCurrentSiteId(), Boolean.valueOf(true)};
+    Object [] values = {"OWN_PUBLISHED_ASSESSMENT", AgentFacade.getCurrentSiteId(), Boolean.valueOf(true)};
     //List list = getHibernateTemplate().find("select new PublishedAssessmentData(a.publishedAssessmentId, count(a)) from AssessmentGradingData a where a.forGrade=? group by a.publishedAssessmentId", Boolean.valueOf(true));
     
     List list = getHibernateTemplate().find(
