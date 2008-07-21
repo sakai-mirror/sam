0046f76ae72fc030f2a26d67581b3a4c7a1b8545
mbanting@ca.ibm.com <mbanting@ca.ibm.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-07-21 07:39:49 +0000

    merge changes from trunk to branch (r48636:49048)
    git-svn-id: https://source.sakaiproject.org/svn/sam/branches/SAK-13408@49056 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentFacadeQueries.java
+++ samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentFacadeQueries.java
@@ -1358,7 +1358,7 @@ public class AssessmentFacadeQueries extends HibernateDaoSupport implements
 			query = "select new AssessmentData(a.assessmentBaseId, a.title, a.lastModifiedDate)"
 					+ " from AssessmentData a, AuthorizationData z where "
 					+ " a.title=? and a.assessmentBaseId!=? and z.functionId=? and "
-					+ " a.assessmentBaseId=z.qualifierId and z.agentIdString=?";
+					+ " a.assessmentBaseId=z.qualifierId and z.agentIdString=? and a.status=?";
 
 			final String hql = query;
 			final String titlef = title;
@@ -1370,6 +1370,7 @@ public class AssessmentFacadeQueries extends HibernateDaoSupport implements
 					q.setLong(1, assessmentBaseId.longValue());
 					q.setString(2, "EDIT_ASSESSMENT");
 					q.setString(3, currentSiteId);
+					q.setInteger(4,1);
 					return q.list();
 				};
 			};
@@ -1435,7 +1436,7 @@ public class AssessmentFacadeQueries extends HibernateDaoSupport implements
 
 	private String fileSizeInKB(int fileSize) {
 		String fileSizeString = "1";
-		int size = Math.round(fileSize / 1024);
+		int size = Math.round((float)fileSize / 1024.0f);
 		if (size > 0) {
 			fileSizeString = size + "";
 		}
