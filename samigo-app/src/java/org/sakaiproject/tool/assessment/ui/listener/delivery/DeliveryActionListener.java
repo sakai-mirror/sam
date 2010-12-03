e81462a7ce8ec66b34c08340ad7c3162300f5164
ktsao@stanford.edu <ktsao@stanford.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-12-03 00:10:06 +0000

    SAM-844
    git-svn-id: https://source.sakaiproject.org/svn/sam/branches/sakai_2-6-x@85902 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
+++ samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
@@ -1158,7 +1158,7 @@ public class DeliveryActionListener
 		}
 
         Collections.shuffle(shuffled, 
-        		new Random( (long) item.getText().hashCode() + agentString.hashCode()));
+        		new Random( (long) item.getText().hashCode() + (getAgentString() + "_" + item.getItemId().toString()).hashCode()));
         /*
         if (item.getTypeId().equals(TypeIfc.MATCHING))
         {
