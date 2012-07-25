f9d1f4aac6fd6a6233a08fe206d59edb10701d02
ktsao@stanford.edu <ktsao@stanford.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2012-07-25 23:28:44 +0000

    NOJIRA. Testing
    git-svn-id: https://source.sakaiproject.org/svn/sam/branches/samigo-2.8.x@110757 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
+++ samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
@@ -20,7 +20,6 @@
  **********************************************************************************/
 
 
-
 package org.sakaiproject.tool.assessment.ui.listener.delivery;
 
 import java.util.ArrayList;
