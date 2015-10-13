5aa83486b97b73a7ef7f6b8aa156c49c73b3849b
enietzel@anisakai.com <enietzel@anisakai.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2015-10-13 21:38:33 +0000

    SAM-2562 merge PR 607 from git
    git-svn-id: https://source.sakaiproject.org/svn/sam/branches/sakai-10.x@321483 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/author/AuthorActionListener.java
+++ samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/author/AuthorActionListener.java
@@ -443,7 +443,7 @@ public class AuthorActionListener
 			  returnValue = false;
 		  }
 
-		  if (retractDate != null && retractDate.before(currentDate)) {
+		  if (acceptLateSubmission && retractDate != null && retractDate.before(currentDate)) {
 			  returnValue = false;
 		  }
 
